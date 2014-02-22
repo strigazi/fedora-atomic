ab4f39855e9768d0145ae5a8b832545deebbc876
Colin Walters <walters@verbum.org>
2014-02-21 20:34:39 -0500

    More work

--- web/app.js
+++ web/app.js
@@ -9,17 +9,19 @@
     fatomic.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/', {
-                templateUrl: 'partials/home.html',
-                controller: 'fatomicHomeCtrl'
-            }).
-            when('/installation', {
-                templateUrl: 'partials/installation.html'
-            }).
+                templateUrl: 'partials/home.html'
+	    }).
             when('/background', {
                 templateUrl: 'partials/background.html'
             }).
+            when('/build-status', {
+                templateUrl: 'partials/build-status.html'
+            }).
             when('/implementation', {
                 templateUrl: 'partials/implementation.html'
+            }).
+            when('/installation', {
+                templateUrl: 'partials/installation.html'
             }).
             otherwise({
                 redirectTo: 'partials/unknown.html',
