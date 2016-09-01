c97ede09528e368a0233dc378e39f1158577037e
Colin Walters <walters@verbum.org>
2016-09-01 14:57:21 -0400

    post: Neuter all systemd PrivateTmp= and Protect(Home|System)
    See https://github.com/systemd/systemd/issues/4082 for the root
    of this (mounts not following symlinks).
    
    For Protect(Home|System), for the most part this is unnecessary
    with ostree (we already have a ro bind mount over /usr).  And
    unfortunately it doesn't cover `/sysroot`.
    
    Anyways, at some point we'll revisit the systemd Protect*, but
    for now let's have the current system boot.

--- treecompose-post.sh
+++ treecompose-post.sh
@@ -5,6 +5,9 @@ set -xeuo pipefail
 # Work around https://bugzilla.redhat.com/show_bug.cgi?id=1265295
 echo 'Storage=persistent' >> /etc/systemd/journald.conf
 
+# Work around https://github.com/systemd/systemd/issues/4082
+find /usr/lib/systemd/system/ -type f -exec sed -i -e '/^PrivateTmp=/d' -e '/^Protect\(Home\|System\)=/d' {} \;
+
 # See: https://bugzilla.redhat.com/show_bug.cgi?id=1051816
 KEEPLANG=en_US
 find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "${KEEPLANG}" -exec rm -rf {} +
