c065aef8ba942087af4e4a1da0555a89749eb612
Jonathan Lebon <jlebon@redhat.com>
2016-09-14 12:58:14 -0400

    treecompose-post.sh: copy, don't move the locale archive
    Most processes, including bash, will have locale-archive mmap'ed.
    Renaming it to .tmpl will cause it to get truncated by
    build-locale-archive. We should make sure that it remains untouched
    so that bash doesn't die with SIGBUS. OTOH, build-locale-archive knows
    to unlink and create a new inode so as to not affect already mmap'ed
    versions.

--- treecompose-post.sh
+++ treecompose-post.sh
@@ -12,5 +12,5 @@ find /usr/lib/systemd/system/ -type f -exec sed -i -e '/^PrivateTmp=/d' -e '/^Pr
 KEEPLANG=en_US
 find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "${KEEPLANG}" -exec rm -rf {} +
 localedef --list-archive | grep -a -v ^"${KEEPLANG}" | xargs localedef --delete-from-archive
-mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
+cp -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 build-locale-archive
